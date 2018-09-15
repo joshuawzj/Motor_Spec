34d76d9c3ae4e85291ab061edf1af4cc39b048eb
joshuazj <zijun.joshua.wang@gmail.com>
2018-09-15 18:07:34 +1000

    completed vehicle list page

--- MotorSpecs/VehicleList/models.py
+++ MotorSpecs/VehicleList/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class VehicleList(models.Model):
+    carID = models.CharField(max_length=10)
+    carMakeName = models.CharField(max_length=50)
+    carModel = models.CharField(max_length=50)
+    carSeries = models.CharField(max_length=50)
+    carSeriesYear = models.CharField(max_length=10)
+    carPriceNew = models.CharField(max_length=15)
+    carEngineSize = models.CharField(max_length=10)
+    carFuelSystem = models.CharField(max_length=50)
+    carTankCapacity = models.CharField(max_length=10)
+    carPower = models.CharField(max_length=10)
+    carSeatingCapacity = models.CharField(max_length=5)
+    carStandardTransmission = models.CharField(max_length=10)
+    carBodyType = models.CharField(max_length=50)
+    carDrive = models.CharField(max_length=10)
+    carWheelbase = models.CharField(max_length=10)
+    def __str__(self):
+        return self.carID + ' - ' + self.carMakeName
--- MotorSpecs/VehicleList/views.py
+++ MotorSpecs/VehicleList/views.py
@@ -1,7 +1,32 @@
 from django.shortcuts import render
+from .models import VehicleList
+import json
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'vehiclelist.html')
+    cars = VehicleList.objects.all()
+    res = []
+    for car in cars:
+        json = {
+            'carID':str(car.carID),
+            'carMakeName':car.carMakeName,
+            'carModel':car.carModel,
+            'carSeries':car.carSeries,
+            'carSeriesYear':car.carSeriesYear,
+            'carPriceNew':car.carPriceNew,
+            'carEngineSize':car.carEngineSize,
+            'carFuelSystem':car.carFuelSystem,
+            'carTankCapacity':car.carTankCapacity,
+            'carPower':car.carPower,
+            'carSeatingCapacity':car.carSeatingCapacity,
+            'carStandardTransmission':car.carStandardTransmission,
+            'carBodyType':car.carBodyType,
+            'carDrive':car.carDrive,
+            'carWheelbase':car.carWheelbase
+        }
+        print(json)
+        res.append(json)
+
+    return render(request, 'vehiclelist.html',{'car_list':res})
 
