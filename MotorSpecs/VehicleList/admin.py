6ce8c6cc394b1d480b0dfca3a9da5059abe35e77
joshuazj <zijun.joshua.wang@gmail.com>
2018-09-16 16:37:04 +1000

    Add register in VehicleList

--- MotorSpecs/VehicleList/admin.py
+++ MotorSpecs/VehicleList/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+class VehicleListAdmin(admin.ModelAdmin):
+    list_display = ['carID','carMakeName','carModel','carSeries','carSeriesYear','carPriceNew','carEngineSize',
+                    'carFuelSystem','carTankCapacity','carPower','carSeatingCapacity','carStandardTransmission',
+                    'carBodyType','carDrive','carWheelbase'
+                    ]
+
+admin.site.register(VehicleList,VehicleListAdmin)
