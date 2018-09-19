d9aa24a196fef12596d941551370c1f6cf9622e7
Brad <38450922+lilpapashaw@users.noreply.github.com>
2018-09-19 20:32:26 +1000

    Linked pages together + added login_required

--- MotorSpecs/DataRepresent/views.py
+++ MotorSpecs/DataRepresent/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from index.models import RentalTrends
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
-
+@login_required
 def rentaltrends(request):
     items = RentalTrends.objects.all()
     res = []
